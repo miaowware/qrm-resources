e16d1f362feefcd3ec8c28193bdf177c8c5ac1f6
0x5c <dev@0x5c.io>
2024-07-29 01:32:33 -0400

    scripts: formatting and mypy

--- gethashes.py
+++ gethashes.py
@@ -2,7 +2,7 @@
 Index file hash tool for qrm-resources
 ---
 
-Copyright (c) 2020, MiaowWare
+Copyright (c) 2020-2024, MiaowWare
 Released under the terms of the BSD 3-Clause license.
 """
 
@@ -20,22 +20,24 @@ backup = basedir / ("~" + index_file.name)
 
 
 print("Reading the index file...")
-with index_file.open("r") as file:
-    index = json.load(file)
+with index_file.open("r") as f:
+    index = json.load(f)
 
     # Creating a backup
     with backup.open("w") as bkfile:
-        file.seek(0)
-        bkfile.write(file.read())
+        f.seek(0)
+        bkfile.write(f.read())
 
 
 print("Getting the hashes...")
+resource: str
+versions: dict[str, list[dict[str, str]]]
 for resource, versions in index["resources"].items():
     for version, files in versions.items():
         for file_idx, file in enumerate(files):
             filepath = basedir / file["filename"]
-            with filepath.open("rb") as file:
-                hashed = hashlib.md5(file.read())
+            with filepath.open("rb") as f:
+                hashed = hashlib.md5(f.read())
             index["resources"][resource][version][file_idx]["hash"] = str(binascii.hexlify(hashed.digest()), "utf-8")
 
 
@@ -43,9 +45,9 @@ index["last_updated"] = datetime.now(UTC).isoformat()
 
 
 print("Writing the hashes...")
-with index_file.open("w", newline="\n") as file:
-    json.dump(index, file, indent=4)
-    file.write("\n")
+with index_file.open("w", newline="\n") as f:
+    json.dump(index, f, indent=4)
+    f.write("\n")
 
 
 # Everything ran fine, remove the backup
